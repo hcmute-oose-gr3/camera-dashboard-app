6c600014f04f0297733c0feea0165236d83419d8
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-04-08 16:13:22 +0700

    style: styling login and some global typography

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -5,15 +5,13 @@ export default () => {
 	const [t] = useI18nContext();
 	return (
 		<div class="mx-auto mt-24 w-fit">
-			<h4 class="text-center">{t.login.header()}</h4>
+			<h1 class="text-center">{t.login.header()}</h1>
 			<form
 				class="flex flex-col gap-y-6 min-w-[300px]
-				bg-fill-300 border-fill-600 border-[1px] rounded-md p-4 mt-8"
+				bg-fill-100 border-fill-300 border-[1px] rounded-md p-4 mt-8"
 			>
 				<div class="flex flex-col gap-y-1">
-					<label for="username">
-						{t.login.usernameLabel()}
-					</label>
+					<label for="username">{t.login.usernameLabel()}</label>
 					<input
 						id="username"
 						name="username"
@@ -22,24 +20,22 @@ export default () => {
 					/>
 				</div>
 				<div class="flex flex-col gap-y-1">
-					<div class="flex justify-between">
-						<label for="password">
-							{t.login.passwordLabel()}
-						</label>
+					<div class="flex justify-between items-center">
+						<label for="password">{t.login.passwordLabel()}</label>
 						<a href="#">{t.login.forgotPassword()}</a>
 					</div>
 					<input
 						id="password"
 						name="password"
-						type="text"
+						type="password"
 						placeholder={t.login.passwordPlaceholder()}
 					/>
 				</div>
-				<Button type="button" class="bg-primary-500 text-stroke-50">
+				<Button type="button" class="bg-primary-700 text-fill-100">
 					{t.login.login()}
 				</Button>
 			</form>
-			<div class="bg-fill-300 border-fill-500 border-[1px] rounded-md p-4 mt-4">
+			<div class="bg-fill-200/20 border-fill-300 border-[1px] rounded-md p-4 mt-4">
 				<span>{t.login.noAccount()}</span>{' '}
 				<a href="#">{t.login.signUp()}</a>.
 			</div>
