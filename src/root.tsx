b9964102b9c9f89208f43ee8c4f5b31d221cf33e
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-04-08 15:52:40 +0700

    feat(tailwind): default styling

--- src/root.tsx
+++ src/root.tsx
@@ -24,6 +24,16 @@ export default function Root() {
 					name="viewport"
 					content="width=device-width, initial-scale=1"
 				/>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link
+					rel="preconnect"
+					href="https://fonts.gstatic.com"
+					crossorigin="anonymous"
+				/>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
+					rel="stylesheet"
+				/>
 			</Head>
 			<Body class="bg-fill-50">
 				<Suspense>
