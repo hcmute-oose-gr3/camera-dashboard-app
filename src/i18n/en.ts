fec4d9020f443728c67008755d2861bb4c236fde
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-04-10 13:49:01 +0700

    feat(login): handle form validation, form submit

--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -1,10 +1,19 @@
 export default {
 	login: {
 		header: 'Sign in to camera dashboard',
-		usernameLabel: 'Username',
-		usernamePlaceholder: 'Enter your username',
-		passwordLabel: 'Password',
-		passwordPlaceholder: 'Enter your password',
+		username: {
+			label: 'Username',
+			placeholder: 'Enter your username',
+			min: ({ length }: { length: number }) =>
+				`Username must be at least ${length} characters long`,
+			regex: 'Username can only contain letters and numbers',
+		},
+		password: {
+			label: 'Password',
+			placeholder: 'Enter your password',
+			min: ({ length }: { length: number }) =>
+				`Password must be at least ${length} characters long`,
+		},
 		login: 'Sign in',
 		forgotPassword: 'Forgot password?',
 		noAccount: "Don't have an account?",
--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,44 +1,131 @@
+import { createSignal, Show } from 'solid-js';
+import { A, createRouteAction } from 'solid-start';
 import Button from '~/components/Button';
 import { useI18nContext } from '~/components/I18nProvider';
+import SpinnerPending from '~/components/SpinnerPending';
+import { z } from 'zod';
+import Input from '~/components/Input';
+import { APIErrorResponse, APIResponse } from '~/models/api-response';
+import { instanceOf } from '~/utils';
+
+type FormFields = 'username' | 'password';
 
 export default () => {
 	const [t] = useI18nContext();
+	const [logging, setLogging] = createSignal(false);
+	const [fieldErrors, setFieldErrors] = createSignal<
+		Partial<Record<FormFields, string[] | null>>
+	>({});
+	const [formError, setFormError] = createSignal<
+		APIErrorResponse['error'] | undefined
+	>();
+	const [_, { Form }] = createRouteAction(async (formData: FormData) => {
+		setLogging(true);
+
+		const schema = z.object({
+			username: z.coerce
+				.string()
+				.trim()
+				.min(3, t.login.username.min({ length: 3 }))
+				.regex(/[a-zA-Z0-9]+/, t.login.username.regex()),
+			password: z.coerce
+				.string()
+				.min(3, t.login.password.min({ length: 6 })),
+		});
+		const ret = await schema.safeParseAsync({
+			username: formData.get('username'),
+			password: formData.get('password'),
+		});
+
+		if (!ret.success) {
+			setFieldErrors(ret.error.formErrors.fieldErrors);
+			setLogging(false);
+			return;
+		}
+
+		setFieldErrors({});
+		formData.set('username', ret.data.username);
+
+		const json = await fetch('/api/v1/login', {
+			method: 'post',
+			body: formData,
+		})
+			.then((v) => v.json() as Promise<APIResponse>)
+			.finally(() => {
+				setLogging(false);
+			});
+
+		if (instanceOf<APIErrorResponse>(json, 'error')) {
+			setFormError(json.error);
+			return;
+		}
+		setFormError();
+	});
 	return (
-		<div class="mx-auto mt-24 w-fit">
-			<h1 class="text-center">{t.login.header()}</h1>
-			<form
-				class="flex flex-col gap-y-6 min-w-[300px]
-				bg-fill-100 border-fill-300 border-[1px] rounded-md p-4 mt-8"
-			>
-				<div class="flex flex-col gap-y-1">
-					<label for="username">{t.login.usernameLabel()}</label>
-					<input
-						id="username"
-						name="username"
-						type="text"
-						placeholder={t.login.usernamePlaceholder()}
-					/>
-				</div>
-				<div class="flex flex-col gap-y-1">
-					<div class="flex justify-between items-center">
-						<label for="password">{t.login.passwordLabel()}</label>
-						<a href="#">{t.login.forgotPassword()}</a>
+		<>
+			<h1 class="text-center mx-auto mt-24">{t.login.header()}</h1>
+			<div class="mx-auto w-min mt-6">
+				<Form
+					class="flex flex-col gap-y-6 min-w-[400px]
+					bg-fill-100 border-fill-300 border-[1px] rounded-lg p-4 mt-8"
+				>
+					<div class="flex flex-col gap-y-1">
+						<Input
+							id="username"
+							name="username"
+							type="text"
+							spellcheck={false}
+							placeholder={t.login.username.placeholder()}
+							class="transition"
+							label={t.login.username.label()}
+							accent={
+								fieldErrors().username?.[0]
+									? 'negative'
+									: undefined
+							}
+							caption={fieldErrors().username?.[0]}
+						/>
 					</div>
-					<input
-						id="password"
-						name="password"
-						type="password"
-						placeholder={t.login.passwordPlaceholder()}
-					/>
+					<div class="flex flex-col gap-y-1 relative">
+						<a href="#" class="absolute right-0 top-0">
+							{t.login.forgotPassword()}
+						</a>
+						<Input
+							id="password"
+							name="password"
+							type="password"
+							placeholder={t.login.password.placeholder()}
+							class="transition"
+							label={t.login.password.label()}
+							accent={
+								fieldErrors().password?.[0]
+									? 'negative'
+									: undefined
+							}
+							caption={fieldErrors().password?.[0]}
+						/>
+					</div>
+					<Button type="submit" disabled={logging()}>
+						<SpinnerPending when={logging}>
+							{t.login.login()}
+						</SpinnerPending>
+					</Button>
+				</Form>
+				<div class="bg-fill-200/20 border-fill-300 border-[1px] rounded-md p-4 mt-4">
+					<span>{t.login.noAccount()}</span>{' '}
+					<A href="/signup">{t.login.signUp()}</A>.
 				</div>
-				<Button type="button" class="bg-primary-700 text-fill-100">
-					{t.login.login()}
-				</Button>
-			</form>
-			<div class="bg-fill-200/20 border-fill-300 border-[1px] rounded-md p-4 mt-4">
-				<span>{t.login.noAccount()}</span>{' '}
-				<a href="#">{t.login.signUp()}</a>.
+				<Show when={!!formError()}>
+					<div class="bg-negative-300 border-negative-400 border-[1px] rounded-md p-4 mt-4">
+						<p>
+							<span class="text-negative-700 text-h5">
+								{formError()!.code}
+							</span>{' '}
+							{formError()!.message}.
+						</p>
+					</div>
+				</Show>
 			</div>
-		</div>
+		</>
 	);
 };
