8293263d43441ea76c63cbb2eaae5d1ecfe5653a
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-04-11 15:34:34 +0700

    feat(components): add Button, Input, Spinner components

---
+++ src/lib/components/Input.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	export let accent: 'positive' | 'negative' | undefined = undefined;
+	export let label: string | undefined = undefined;
+	export let caption: string | undefined = undefined;
+</script>
+
+{#if label}
+	<label for={$$restProps.id} class={accent ? `label-${accent}` : undefined}>
+		{label}
+	</label>
+{/if}
+<input
+	{...$$restProps}
+	class={'active:scale-[0.99] active:ring-0 active:ring-offset-0 ease-in' +
+		(accent ? `input-${accent}` : '')}
+/>
+{#if caption}
+	<div class={accent ? `caption-${accent}` : undefined}>
+		{caption}
+	</div>
+{/if}
+
+<style lang="postcss">
+	.label-negative {
+		@apply text-negative-700;
+	}
+
+	.label-positive {
+		@apply text-positive-700;
+	}
+
+	.input-negative {
+		@apply border-negative-700;
+	}
+
+	.input-positive {
+		@apply border-positive-700;
+	}
+
+	.caption-negative {
+		@apply text-negative-700;
+	}
+
+	.caption-positive {
+		@apply text-positive-700;
+	}
+</style>
