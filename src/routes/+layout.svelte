1a0dd91c9c852da13e1b2d6a199809a7d9e12414
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-04-13 14:44:46 +0700

    feat(routes): add route transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,17 @@
 <script lang="ts">
+	import { blur } from 'svelte/transition';
 	import '../app.css';
+	import { quadIn, quadOut } from 'svelte/easing';
+	import type { LayoutData } from './$types';
+	export let data: LayoutData;
 </script>
 
-<slot />
+{#key data.url.href}
+	<section
+		in:blur={{ amount: 2, duration: 200, delay: 200, easing: quadIn }}
+		out:blur|local={{ amount: 1, duration: 400, easing: quadOut }}
+		class="absolute w-full h-full"
+	>
+		<slot />
+	</section>
+{/key}
