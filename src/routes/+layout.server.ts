b784b1c777f13ebc71a10b0e16009e2b23a1cbc1
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-04-13 14:45:20 +0700

    refactor(+layout.server): return whole locals object to client

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,5 +1,5 @@
 import type { LayoutServerLoad } from './$types';
 
-export const load: LayoutServerLoad = ({ locals: { locale } }) => {
-	return { locale };
+export const load: LayoutServerLoad = ({ locals }) => {
+	return locals;
 };
