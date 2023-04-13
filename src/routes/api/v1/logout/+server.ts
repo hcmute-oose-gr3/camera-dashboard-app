ad05461e822e9737908a2da0f580542975551d63
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-04-13 15:04:45 +0700

    feat(logout): logout should unset sessionToken in database

--- src/routes/api/v1/logout/+server.ts
+++ src/routes/api/v1/logout/+server.ts
@@ -1,12 +1,27 @@
 import httpStatus from 'http-status';
+import { ObjectId } from 'mongodb';
 import apiResponder from '~/lib/server/services/api-responder';
+import mongodbCollections from '~/lib/server/services/mongodb-collections.js';
 
 export const GET = async (e) => {
+	if (!e.locals.userId) {
+		return apiResponder.error({
+			error: { code: httpStatus.UNAUTHORIZED, message: 'You are not logged in yet' }
+		});
+	}
 	e.cookies.delete('session_token', {
 		path: '/',
 		httpOnly: true,
 		sameSite: 'strict',
 		secure: process.env.NODE_ENV === 'production'
 	});
+	try {
+		await mongodbCollections.users.updateOne(
+			{ _id: new ObjectId(e.locals.userId) },
+			{ $unset: { sessionToken: true } }
+		);
+	} catch (e) {
+		throw e;
+	}
 	return apiResponder.data({ data: {} }, { status: httpStatus.OK });
 };
--- src/routes/logout/+page.ts
+++ src/routes/logout/+page.ts
@@ -3,10 +3,7 @@ import type { PageLoad } from './$types';
 import { APIRoutes } from '~/lib/utils/api-routes';
 
 export const load = (async ({ fetch }) => {
-	try {
-		await fetch(APIRoutes.LOGOUT, { method: 'get' });
-	} catch (e) {
-		throw e;
-	}
-	throw redirect(302, '/');
+	await fetch(APIRoutes.LOGOUT, { method: 'get' }).finally(() => {
+		throw redirect(302, '/');
+	});
 }) satisfies PageLoad;
