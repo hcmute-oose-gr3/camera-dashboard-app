c673b01356a187cdd167ef3094a0a39121e3e0e6
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-04-17 00:04:05 +0700

    feat(components): add logo components

---
+++ src/lib/components/LogoType.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import LL from '~/i18n/i18n-svelte';
+	import Logo from './Logo.svelte';
+</script>
+
+<div class="flex items-center justify-center gap-x-4 text-primary-700 {$$restProps.class}">
+	<Logo />
+	<h1 class="flex flex-col items-end uppercase">
+		{#each $LL.logo.name().split(' ') as word}
+			<span>{word}</span>
+		{/each}
+	</h1>
+</div>
