e69d3b69b66ca0a3fede8ad109948f4e19a5c3b9
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-04-22 01:49:02 +0700

    feat(routes):  persist lang search params across navigation in root layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,12 @@
 <script lang="ts">
-	import { blur } from 'svelte/transition';
-	import '../app.css';
-	import { quadIn, quadOut } from 'svelte/easing';
+	import { beforeNavigate } from '$app/navigation';
+	import '~/app.css';
 	import type { LayoutData } from './$types';
+
 	export let data: LayoutData;
+	beforeNavigate((nav) => {
+		nav.to?.url.searchParams.set('lang', data.locale);
+	});
 </script>
 
-{#key data.url.href}
-	<section
-		in:blur={{ amount: 2, duration: 200, delay: 200, easing: quadIn }}
-		out:blur|local={{ amount: 1, duration: 400, easing: quadOut }}
-		class="absolute w-full h-full"
-	>
-		<slot />
-	</section>
-{/key}
+<slot />
