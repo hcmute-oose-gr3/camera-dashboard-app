105c60b1e3af9dbfe113becd010efc217e269576
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-04-23 06:25:55 +0700

    refactor: no longer persist lang query parameter

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,5 @@
 <script lang="ts">
-	import { beforeNavigate } from '$app/navigation';
 	import '~/app.css';
-	import type { LayoutData } from './$types';
-
-	export let data: LayoutData;
-	beforeNavigate((nav) => {
-		nav.to?.url.searchParams.set('lang', data.locale);
-	});
 </script>
 
 <slot />
