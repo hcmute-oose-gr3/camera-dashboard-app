716680eab0ff530f0943d7bfa37c4ba9a9a7f75d
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-04-23 06:26:37 +0700

    feat: store language in local storage; try to detect best language

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,9 +1,25 @@
 import type { LayoutLoad } from './$types';
-import { loadLocaleAsync } from '../i18n/i18n-util.async';
-import { setLocale } from '../i18n/i18n-svelte';
+import { loadLocaleAsync } from '~/i18n/i18n-util.async';
+import { setLocale } from '~/i18n/i18n-svelte';
+import { browser } from '$app/environment';
+import { isLocale } from '~/i18n/i18n-util';
+import type { Locales } from '~/i18n/i18n-types';
 
 export const load: LayoutLoad = async ({ data, url }) => {
-	await loadLocaleAsync(data.locale);
-	setLocale(data.locale);
+	if (browser) {
+		let storedLanguage = localStorage.getItem('lang') as Locales | null;
+		if (!storedLanguage || !isLocale(storedLanguage)) {
+			storedLanguage = data.locale;
+		}
+		let language = (url.searchParams.get('lang') as Locales | null) || storedLanguage;
+
+		data.locale = language;
+		await loadLocaleAsync(language);
+		if (storedLanguage !== language) {
+			localStorage.setItem('lang', language);
+		}
+		document.getElementsByTagName('html')[0]?.setAttribute('lang', language);
+		setLocale(language);
+	}
 	return { ...data, url };
 };
