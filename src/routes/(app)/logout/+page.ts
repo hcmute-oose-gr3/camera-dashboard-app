cedd0ffea9f0ee55b44e66340900e9bfce5e83f8
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-04-23 06:41:28 +0700

    refactor(logout): use httpStatus

--- src/routes/(app)/logout/+page.ts
+++ src/routes/(app)/logout/+page.ts
@@ -1,9 +1,10 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 import { ApiRoutes } from '~/lib/utils/api-routes';
+import httpStatus from 'http-status';
 
 export const load = (async ({ fetch }) => {
 	await fetch(ApiRoutes.LOGOUT, { method: 'get' }).finally(() => {
-		throw redirect(302, '/');
+		throw redirect(httpStatus.FOUND, '/');
 	});
 }) satisfies PageLoad;
