a0673fab0c840b8674a6867acb135af716c66e41
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-04-26 15:22:15 +0700

    feat(i18n): add shortDate formatter

--- src/i18n/formatters.ts
+++ src/i18n/formatters.ts
@@ -1,11 +1,16 @@
-import type { FormattersInitializer } from 'typesafe-i18n'
-import type { Locales, Formatters } from './i18n-types'
+import type { FormattersInitializer } from 'typesafe-i18n';
+import type { Locales, Formatters } from './i18n-types';
+import { date } from 'typesafe-i18n/formatters';
 
 export const initFormatters: FormattersInitializer<Locales, Formatters> = (locale: Locales) => {
-
 	const formatters: Formatters = {
-		// add your formatter functions here
-	}
+		shortDate: date(locale, {
+			weekday: 'long',
+			day: 'numeric',
+			month: 'short',
+			year: 'numeric'
+		})
+	};
 
-	return formatters
-}
+	return formatters;
+};
