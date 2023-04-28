78d039a9de527c5402fd8ee458fc844c3cb1dd1e
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-04-29 04:35:47 +0700

    feat(components): add NavigationProgressBar component

---
+++ src/lib/components/NavigationProgressBar.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import { navigating } from '$app/stores';
+	import { sineIn, sineOut } from 'svelte/easing';
+	import { tweened } from 'svelte/motion';
+
+	const progress = tweened(0);
+	let width: number;
+	let hidden = false;
+	let hiddenTimeout = 0;
+
+	$: {
+		if ($navigating) {
+			progress.set(0, { duration: 0 }).finally(() => {
+				if ($navigating) {
+					progress.set(0.7, { duration: 6000, easing: sineOut });
+				}
+			});
+		} else {
+			progress.set(1, { duration: 200, easing: sineIn });
+		}
+	}
+
+	$: {
+		if (!$navigating) {
+			if (hiddenTimeout) {
+				clearTimeout(hiddenTimeout);
+			}
+			hiddenTimeout = setTimeout(() => {
+				hidden = true;
+			}, 500) as any as number;
+		} else {
+			hidden = false;
+		}
+	}
+	$: width = $progress * (globalThis?.innerWidth || 0);
+</script>
+
+<div
+	class="w-screen absolute transition-transform top-0 left-0 duration-700 {hidden
+		? '-translate-y-[200%]'
+		: ''}"
+>
+	<div class="w-full h-1 bg-primary-400" />
+	<div class="fixed inset-0 h-1 bg-primary-700" style="width: {width}px;" />
+</div>
