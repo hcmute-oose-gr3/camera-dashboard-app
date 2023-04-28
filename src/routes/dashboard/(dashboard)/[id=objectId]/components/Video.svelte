2e428fd631e797b7364c97fe946715d65735ec02
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-04-29 01:48:19 +0700

    refactor(Video): remove log

--- src/routes/dashboard/(dashboard)/[id=objectId]/components/Video.svelte
+++ src/routes/dashboard/(dashboard)/[id=objectId]/components/Video.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
 	export let type: string;
 	export let src: string;
-
-	$: console.log();
 </script>
 
 <video {...$$restProps} class="p-0.5 w-full shadow-lg bg-fill-100 rounded-[2rem]">
