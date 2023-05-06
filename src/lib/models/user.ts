49fff5330d419a40f7ad209a06feddb67b6d68aa
NNQA <anhla12h@gmail.com>
2023-05-06 13:01:43 +0700

    refactor models user,add unverified

--- src/lib/models/user.ts
+++ src/lib/models/user.ts
@@ -3,6 +3,7 @@ import type { Document, ObjectId } from 'mongodb';
 export default interface User extends Document, Partial<UserData> {
 	email: string;
 	password: string;
+	unverified?: boolean;
 }
 
 interface UserData {
