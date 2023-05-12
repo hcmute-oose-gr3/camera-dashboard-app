303c113c465100364f55a7c75308187ebe86403f
NNQA <anhla12h@gmail.com>
2023-05-12 18:39:22 +0700

    add api route

--- src/lib/utils/api-routes.ts
+++ src/lib/utils/api-routes.ts
@@ -2,6 +2,6 @@ export const enum ApiRoutes {
 	LOGIN = '/api/v1/login',
 	LOGOUT = '/api/v1/logout',
 	SIGNUP = '/api/v1/signup',
-	VERIFY = '/api/v1/verify'
-	
+	VERIFY = '/api/v1/verify',
+	AREA = '/api/v1/area'
 }
