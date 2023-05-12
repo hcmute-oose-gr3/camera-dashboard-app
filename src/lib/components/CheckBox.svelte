f76d7c33f155f4f50205430100c226bd243d2095
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-05-12 23:43:02 +0700

    refactor(CheckBox): merge classes

---
+++ src/lib/components/CheckBox.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { twMerge } from 'tailwind-merge';
+	export let checked = false;
+</script>
+
+<input
+	type="checkbox"
+	on:pointerdown
+	on:input
+	bind:checked
+	{...$$restProps}
+	class={twMerge(
+		'form-checkbox text-primary-700 focus:ring-primary-500 bg-fill-100 transition rounded focus:ring-offset-1 focus:ring',
+		$$restProps.class
+	)}
+/>
