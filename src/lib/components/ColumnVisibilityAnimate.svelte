7fd601893b9eee8856bd04b0c37a1bfbae066c34
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-05-13 03:23:48 +0700

    feat(components): add Radio and visibility animate components

---
+++ src/lib/components/ColumnVisibilityAnimate.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let visible = false;
+</script>
+
+<div
+	class="grid ease-in-out duration-300 transform-[grid-template-cols] {visible
+		? 'grid-cols-[0fr]'
+		: 'grid-cols-[1fr]'}"
+>
+	<div class="overflow-hidden">
+		<slot />
+	</div>
+</div>
