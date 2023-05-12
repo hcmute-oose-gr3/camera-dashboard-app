ac2ea4798b6a43a17c1f85e236f7624b0c5d44e6
NNQA <anhla12h@gmail.com>
2023-05-12 16:07:17 +0700

    feat: add model area

--- src/lib/models/dashboard.ts
+++ src/lib/models/dashboard.ts
@@ -5,10 +5,15 @@ export interface Dashboard extends Document {
 	owner: ObjectId;
 	name: string;
 	cameras?: WithId<Camera>[];
+	areas?: WithId<Area>[];
 }
 
 interface Camera {
 	updatedAt: Date;
 	name: string;
 	url: string;
+}
+export interface Area {
+	name: string;
+	activate: boolean;
 }
