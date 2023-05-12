6e9d620b6500c951d7a2f7b35c2c3c47b6bc809f
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-05-13 03:23:27 +0700

    feat(Area): add cameras field

--- src/lib/models/dashboard.ts
+++ src/lib/models/dashboard.ts
@@ -4,16 +4,30 @@ export interface Dashboard extends Document {
 	updatedAt: Date;
 	owner: ObjectId;
 	name: string;
-	cameras?: WithId<Camera>[];
 	areas?: WithId<Area>[];
 }
 
-interface Camera {
-	updatedAt: Date;
-	name: string;
-	url: string;
-}
 export interface Area {
 	name: string;
 	activate: boolean;
+	cameras?: WithId<Camera>[];
+}
+
+export interface Camera {
+	updatedAt: Date;
+	name: string;
+	url: string;
+	connection: CameraConnection;
+	securityLevel: CameraSecurityLevel;
+}
+
+export enum CameraConnection {
+	Disconnected,
+	Connected
+}
+
+export enum CameraSecurityLevel {
+	Low,
+	Medium,
+	High
 }
