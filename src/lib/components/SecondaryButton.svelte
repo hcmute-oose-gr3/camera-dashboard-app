7dcb79af41fc46e89ea48762baad93a42f0cbb49
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-05-15 03:20:49 +0700

    style(SecondaryButton): add padding

--- src/lib/components/SecondaryButton.svelte
+++ src/lib/components/SecondaryButton.svelte
@@ -7,7 +7,7 @@
 	on:pointerdown
 	{...$$restProps}
 	class={twMerge(
-		'group {padding} rounded-md text-fill-900 bg-fill-100 border-fill-500 border transition ease-in disabled:bg-fill-300 hover:bg-fill-200 active:scale-[0.99] active:rotate-1 active:bg-fill-300 active:ring-0 active:ring-offset-0 focus:outline-none focus:ring focus:ring-offset-1 focus:ring-primary-500',
+		'group px-4 py-1 rounded-md text-fill-900 bg-fill-100 border-fill-500 border transition ease-in disabled:bg-fill-300 hover:bg-fill-200 active:scale-[0.99] active:rotate-1 active:bg-fill-300 active:ring-0 active:ring-offset-0 focus:outline-none focus:ring focus:ring-offset-1 focus:ring-primary-500',
 		$$restProps.class
 	)}
 >
