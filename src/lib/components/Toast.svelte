3d69c2340c2e85149024d511ae85ccfdf0b75157
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-05-15 06:11:21 +0700

    feat(components): add Toast component

---
+++ src/lib/components/Toast.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { IconName } from 'svelted-heroicons';
+	import Icon from './Icon.svelte';
+	import { twMerge } from 'tailwind-merge';
+
+	export let icon: IconName;
+	export let iconClass: string = '';
+	export let text: string;
+	export let textClass: string = '';
+</script>
+
+<div class="flex">
+	<span class={twMerge('p-2 bg-positive-500 block text-fill-100 rounded-l-md', iconClass)}>
+		<Icon name={icon} class="h-full" />
+	</span>
+	<p class={twMerge('px-2 bg-positive-200 rounded-r-md flex items-center', textClass)}>
+		{text}
+	</p>
+</div>
