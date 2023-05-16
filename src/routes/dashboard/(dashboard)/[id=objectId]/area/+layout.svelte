28ed71e7cf5e4ca0429845992377242707a4cb21
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-05-16 08:31:58 +0700

    feat(area): add layout transition

---
+++ src/routes/dashboard/(dashboard)/[id=objectId]/area/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { backIn, backOut } from 'svelte/easing';
+
+	import { scale } from 'svelte/transition';
+	import type { LayoutData } from './$types';
+	export let data: LayoutData;
+</script>
+
+{#key data.url.pathname.split('/', 6).slice(4, -1).join('/')}
+	<div
+		in:scale|local={{ start: 0.99, duration: 400, delay: 100, easing: backOut }}
+		out:scale|local={{ start: 1.01, duration: 100, easing: backIn }}
+	>
+		<slot />
+	</div>
+{/key}
