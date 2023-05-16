3d5acd05ba5efa5ebc699cccd12f9d39c96aa4a2
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-05-16 14:57:59 +0700

    feat(lib): add Pending component

---
+++ src/lib/components/Pending.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let pending: boolean;
+</script>
+
+<div class="relative overflow-hidden">
+	<div class="absolute transition duration-200 ease-in-out {!pending ? 'scale-0 opacity-0' : ''}">
+		<slot name="pending" />
+	</div>
+	<div
+		class="transition duration-200 text-icon-base ease-in-out {pending
+			? 'scale-0 opacity-0'
+			: ''}"
+	>
+		<slot />
+	</div>
+</div>
