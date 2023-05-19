8d004de6412e982f45e64dd7dcac5355cbccfe54
NNQA <anhla12h@gmail.com>
2023-05-19 14:48:53 +0700

    delete server in modal area

--- src/routes/dashboard/(dashboard)/[id=objectId]/area/modal/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import type { PageServerLoad } from './$types';
-import { ObjectId, type WithId } from 'mongodb';
-import type { Area } from '~/lib/models/dashboard';
-import { DashboardRepository } from '~/lib/server/repositories/dashboard-repository';
-
-export const load = (async ({ params, depends }) => {
-	return {};
-}) satisfies PageServerLoad;
