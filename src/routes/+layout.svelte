fe01adec97e32e81fe8cf3e9bd986dd20bab7578
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-05-19 14:34:05 +0700

    refactor: move page meta to top level

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
 	import '~/app.css';
 	import NavigationProgressBar from '~/lib/components/NavigationProgressBar.svelte';
+	import type { PageMeta } from '.';
+	import { setContext } from 'svelte';
+	import { writable } from 'svelte/store';
+	const meta = writable<PageMeta>({ title: '' });
+	setContext('meta', meta);
+	$: ({ title } = $meta);
 </script>
+
+<svelte:head>
+	<title>{title}</title>
+</svelte:head>
 
 <slot />
 <NavigationProgressBar />
