1db6660f1af8ca40a43c273c4c47217c19dbf814
TangSan003 <20110549@student.hcmute.edu.vn>
2023-05-22 14:35:52 +0700

    add diolog for page setting

--- src/routes/dashboard/(dashboard)/[id=objectId]/setting/components/Dialog.svelte
+++ src/routes/dashboard/(dashboard)/[id=objectId]/setting/components/Dialog.svelte
@@ -2,13 +2,25 @@
 	import { page } from '$app/stores';
 	import Icon from '~/lib/components/Icon.svelte';
 	import type { PageData } from '../$types';
+	import { goto } from '$app/navigation';
+	import LL from '~/i18n/i18n-svelte';
 
 	export let showModal: boolean;
 	export let data: PageData;
 
 	let dialog: HTMLDialogElement;
 	$: if (dialog && showModal) dialog.showModal();
-	// $: console.log(data);
+	async function handleDelete() {
+		const json = await fetch(`/api/v1/dashboard/${data.dashoardId}`, {
+			method: 'DELETE',
+		})
+			.then((r) => {
+				goto('/dashboard');
+			})
+			.catch((e) => {
+				console.log(e);
+			});
+	}
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
@@ -21,7 +33,7 @@
 	<div class="py-4">
 		<div class="flex relative justify-between items-center pb-2 px-8">
 			<div class="flex items-center space-x-3">
-				<h4>Delete</h4>
+				<h4>{$LL.dashboard.setting.modal.title()}</h4>
 				<div class="flex items-center">
 					<h4>
 						{data.user?.email}
@@ -46,13 +58,15 @@
 				<div class="text-opacity-10 text-center font-medium flex items-center space-x-2">
 					<Icon name="Square2Stack" />
 					<p class="text-xs">
-						{data.numberOfitems[0].numberOfAreas} Areas
+						{data.numberOfitems[0].numberOfAreas}
+						{$LL.dashboard.setting.modal.items.area()}
 					</p>
 				</div>
 				<div class="text-opacity-10 text-center font-medium flex items-center space-x-2">
 					<Icon name="VideoCamera" />
 					<p class="text-xs">
-						{data.numberOfitems[0].numberOfCameras} Cameras
+						{data.numberOfitems[0].numberOfCameras}
+						{$LL.dashboard.setting.modal.items.camera()}
 					</p>
 				</div>
 			</div>
@@ -62,8 +76,8 @@
 			<div
 				class="w-[80%] border-[1px] hover:shadow-lg border-fill-1000 border-opacity-20 hover:border-opacity-80 mx-auto text-center rounded-lg"
 			>
-				<button class="p-2 rounded-lg">
-					<p class="text-base">i want to delete this dashboard</p>
+				<button class="p-2 rounded-lg" on:click={() => handleDelete()}>
+					<p class="text-base">{$LL.dashboard.setting.modal.button()}</p>
 				</button>
 			</div>
 		</div>
