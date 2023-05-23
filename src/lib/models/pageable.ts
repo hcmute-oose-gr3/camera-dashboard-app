2717de49566dbf3941a634ee786f76f2ed5f98ad
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-05-24 03:30:38 +0700

    fix(pageable): null can parse to 0

--- src/lib/models/pageable.ts
+++ src/lib/models/pageable.ts
@@ -1,15 +1,12 @@
-class Pageable {
+export class Pageable {
 	private _page: number;
 	private _size: number;
 
 	constructor(page: any, size: any) {
 		page = Number(page);
-		size = Number(size);
+		size = Number(size) || 200;
 		if (isNaN(page)) {
 			page = 0;
-		}
-		if (isNaN(size)) {
-			size = 200;
 		}
 		this._page = page;
 		this._size = size;
