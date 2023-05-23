6a0f72ef894e8123a2a27064d494b13c5de49a7f
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-05-24 03:19:25 +0700

    refactor(area): remove layout load

--- src/routes/dashboard/(dashboard)/[id=objectId]/area/(area)/+layout.ts
+++
@@ -1,7 +0,0 @@
-import { get } from 'svelte/store';
-import type { LayoutLoad } from './$types';
-import LL from '~/i18n/i18n-svelte';
-
-export const load = (() => {
-	return { meta: { title: get(LL).dashboard.area.meta.title() } };
-}) satisfies LayoutLoad;
