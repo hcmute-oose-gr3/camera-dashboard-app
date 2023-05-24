04dc429873b315e3e3e677fdede6858597cb0c71
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-05-25 04:48:02 +0700

    style(TopRight): remove icons

--- src/routes/dashboard/(dashboard)/components/TopRight.svelte
+++ src/routes/dashboard/(dashboard)/components/TopRight.svelte
@@ -5,9 +5,9 @@
 	export let data: TopRightData;
 </script>
 
-<div class="flex items-center gap-x-4">
-	<Icon name="MagnifyingGlass" />
-	<Icon name="Bell" />
+<div class="items-center gap-x-4 hidden md:flex">
+	<!-- <Icon name="MagnifyingGlass" />
+	<Icon name="Bell" /> -->
 	<p>{data.email}</p>
 	<div class="aspect-square rounded-full w-14 h-14 bg-primary-600" />
 </div>
