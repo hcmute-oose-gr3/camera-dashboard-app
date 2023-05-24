baff76bc01aa896b4e2eb3644038aaba0dda458c
NNQA <anhla12h@gmail.com>
2023-05-24 09:12:15 +0700

    add chart

--- src/routes/dashboard/(dashboard)/[id=objectId]/statistic/+page.svelte
+++ src/routes/dashboard/(dashboard)/[id=objectId]/statistic/+page.svelte
@@ -2,14 +2,12 @@
 	import type { PageData } from './$types';
 	import LL from '~/i18n/i18n-svelte';
 	import { getContext } from 'svelte';
-	import type { WritablePageMeta } from '../..';
-	import Table from './components/Chart.svelte';
-	import { warning } from './components/data';
-
-	// export let data: PageData;
-	let checked = Array(warning.length).fill(false);
+	import type { WritablePageMeta } from '~/routes';
+	import Chart from './components/Chart.svelte';
 
 	const meta = getContext('meta') as WritablePageMeta;
 
 	$: $meta.title = $LL.statistic.title();
 </script>
+
+<Chart />
---
+++ src/routes/dashboard/(dashboard)/[id=objectId]/statistic/components/Chart.svelte
@@ -0,0 +1,103 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	let div: HTMLDivElement;
+	onMount(async () => {
+		const ApexCharts = await import('apexcharts').then((r) => r.default);
+		const chart = new ApexCharts(div, {
+			series: [
+				{
+					data: [2.3, 3.1, 4.0, 10.1, 4.0, 3.6, 3.2, 2.3, 1.4, 0.8, 0.5, 0.2],
+				},
+			],
+			chart: {
+				height: 500,
+				type: 'bar',
+			},
+			plotOptions: {
+				bar: {
+					borderRadius: 10,
+					dataLabels: {
+						position: 'top', // top, center, bottom
+					},
+				},
+			},
+			dataLabels: {
+				enabled: true,
+				formatter: function (val: number) {
+					return val + '%';
+				},
+				offsetY: -20,
+				style: {
+					fontSize: '12px',
+					colors: ['#304758'],
+				},
+			},
+
+			xaxis: {
+				categories: [
+					'Jan',
+					'Feb',
+					'Mar',
+					'Apr',
+					'May',
+					'Jun',
+					'Jul',
+					'Aug',
+					'Sep',
+					'Oct',
+					'Nov',
+					'Dec',
+				],
+				position: 'top',
+				axisBorder: {
+					show: false,
+				},
+				axisTicks: {
+					show: false,
+				},
+				crosshairs: {
+					fill: {
+						type: 'gradient',
+						gradient: {
+							colorFrom: '#D8E3F0',
+							colorTo: '#BED1E6',
+							stops: [0, 100],
+							opacityFrom: 0.4,
+							opacityTo: 0.5,
+						},
+					},
+				},
+				tooltip: {
+					enabled: true,
+				},
+			},
+			yaxis: {
+				axisBorder: {
+					show: false,
+				},
+				axisTicks: {
+					show: false,
+				},
+				labels: {
+					show: false,
+					formatter: function (val: number) {
+						return val + '%';
+					},
+				},
+			},
+			title: {
+				text: 'Chart Warning',
+				floating: true,
+				offsetY: 510,
+				align: 'center',
+				style: {
+					color: '#444',
+				},
+			},
+		});
+		chart.render();
+	});
+</script>
+
+<div />
+<div bind:this={div} />
