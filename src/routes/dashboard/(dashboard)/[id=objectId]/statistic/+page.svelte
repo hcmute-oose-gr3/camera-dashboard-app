5c806261e8faed00fef613e1c62727406e35c7c0
NNQA <anhla12h@gmail.com>
2023-05-25 01:44:54 +0700

    add selected are for chart

--- src/routes/dashboard/(dashboard)/[id=objectId]/statistic/+page.svelte
+++ src/routes/dashboard/(dashboard)/[id=objectId]/statistic/+page.svelte
@@ -2,12 +2,22 @@
 	import type { PageData } from './$types';
 	import LL from '~/i18n/i18n-svelte';
 	import { getContext } from 'svelte';
-	import type { WritablePageMeta } from '~/routes';
+	import { meta } from '~/routes';
 	import Chart from './components/Chart.svelte';
-
-	const meta = getContext('meta') as WritablePageMeta;
-
-	$: $meta.title = $LL.statistic.title();
+	import { data } from './components/data';
+	$meta = {
+		title: $LL.statistic.title(),
+	};
+	let selectedOption = Object.keys(data)[0];
 </script>
 
-<Chart />
+<div class="">
+	<select bind:value={selectedOption}>
+		{#each Object.keys(data) as key}
+			<option value={key}>{key}</option>
+		{/each}
+	</select>
+</div>
+<div class="overflow-x-auto">
+	<Chart {selectedOption} />
+</div>
